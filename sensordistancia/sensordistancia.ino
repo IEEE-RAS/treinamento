f8048f1986faf27c517ed05992eeaea22ea39fce
Sergio Pugliesi <sergiopugli@gmail.com>
2024-08-14 09:03:25 -0300

    Atualização de codigo e comentários

--- sensordistancia/sensordistancia.ino
+++ sensordistancia/sensordistancia.ino
@@ -1,46 +1,56 @@
 
 #define DELAY 500
 
-#define PINOA 10
-#define PINOB 13
+#define TRIGGER 2
+#define ECHO 3
+
+#define BUZZER 10
 
 
-int trigger_pin = 2;
-int echo_pin = 3;
-int buzzer_pin = 10;
+// int trigger_pin = 2;
+// int echo_pin = 3;
+// int buzzer_pin = 10;
 int time;
 int distance;
 
 void setup()
 {
-    pinMode(trigger_pin, OUTPUT);
-    pinMode(echo_pin, INPUT);
-    pinMode(buzzer_pin, OUTPUT);
-    pinMode(PINOA, OUTPUT);
-    pinMode(PINOB, OUTPUT);
+    // Configuração dos pinos para o sensor de Distância
+    pinMode(TRIGGER, OUTPUT);
+    pinMode(ECHO, INPUT);
+
+    // Configuração do pino do buzzer
+    pinMode(BUZZER, OUTPUT);
 }
 
 void loop()
 {
-    digitalWrite(trigger_pin, HIGH);
+    // Pulso de gatilho para o sensor de distância (10ms)
+    digitalWrite(TRIGGER, HIGH);
     delayMicroseconds(10);
-    digitalWrite(trigger_pin, LOW);
-    time = pulseIn(echo_pin, HIGH);
+    digitalWrite(TRIGGER, LOW);
+
+    // Leitura do echo do pulso
+    time = pulseIn(ECHO, HIGH);
+
+    // Conversão do tempo de echo em distância em [cm]
     distance = (time * 0.034) / 2;
+
+    // Envia a mensagem de distância via porta serial
+    Serial.print(" Distance= ");
+    Serial.println(distance);
+
+    // Exibe/ativa sinal caso a distância seja menor que 100cm
     if (distance <= 100)
     {
-        Serial.print(" Distance= ");
-        Serial.println(distance);
-        digitalWrite(PINOA, HIGH);
-        digitalWrite(PINOB, HIGH);
-        delay(DELAY);
+        digitalWrite(BUZZER, HIGH);
+        digitalWrite(LED_BUILTIN, HIGH);
     }
     else
     {
-        Serial.print(" Distance= ");
-        Serial.println(distance);
-        digitalWrite(PINOA, LOW);
-        digitalWrite(PINOB, LOW);
-        delay(DELAY);
+        digitalWrite(BUZZER, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
     }
+    
+    delay(DELAY);
 }
